2ac6016f1ebaf1e50e8a037464e6118bcc11c61d
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2022-06-24 20:48:10 -0300

    feat: Add componente time na página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import {Container} from 'react-bootstrap';
+import './assets/css/style.css'
+import { Team } from './components';
 
 export default function App() { 
+  const numberOfTeams = 12
+
   return (
-    <div>Ola</div>
+    <Container>
+      {
+        Array.from(Array(numberOfTeams).keys()).map((item, index) => {
+          return (
+            <Team key={'team-' + index}/>
+          )
+        })
+      }
+    </Container>
   )
 }
