dcfbacee179fb57eb8f1c9fac64926c4e9b180e7
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2022-06-25 18:09:55 -0300

    feat: Salvando dados dos times localmente

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react';
 import {Container} from 'react-bootstrap';
 import './assets/css/style.css'
 import { teams } from './assets/data';
 import { Team } from './components';
 
 export default function App() { 
+  const [teams_, setTeams] = useState(teams)
+
+  useEffect(() => {
+    const teamsExist = localStorage.getItem('teams')
+    if (!teamsExist) localStorage.setItem('teams', JSON.stringify(teams))
+    else setTeams(JSON.parse(localStorage.getItem('teams')))
+  }, [])
 
   return (
     <Container>
       {
-        teams.map((team, index) => {
+        teams_.map((team, index) => {
           return (
             <Team 
               key={'team-' + index}
